0d948180aef72cabc6af1446165e51fc3c517f69
David <davidghazaryan2000@gmail.com>
2020-09-15 08:30:39 +0400

    chat component moved

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Switch, Route, Redirect} from "react-router-dom";
 import Join from "./components/Join";
-import Chat from "./components/Chat";
+import Chat from "./components/Chat/Chat";
 
 export const useRoutes = () => {
     return (
